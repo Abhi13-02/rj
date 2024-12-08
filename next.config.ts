c5f39c0c407330e53f41f64811b7c98c5045c086
C Abhinav Dev <abhinav200513@gmail.com>
2024-12-09 00:37:20 +0530

    initial commit

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    domains: ['https://5e794ca2e8862a9aa01f71d22a4bc140.r2.cloudflarestorage.com'],
+  },
+};
+
+export default nextConfig;

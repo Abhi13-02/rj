9bec050fa75a7eb8e58c7094c3bfa1278652af9a
Pritam Roy <ratsdust4226@gmail.com>
2024-12-25 11:09:34 +0530

    build issues fixed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,29 @@
-import { auth } from "@/auth"
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
 
-const protectedRoutes = ["/dashboard", "/imageUpload"]
+export const runtime = "nodejs"; // Explicitly set the runtime to Node.js
 
-export default auth((req) => {
-  const isLoggedIn = !!req.auth
+const protectedRoutes = ["/dashboard", "/imageUpload"];
+
+export default async function middleware(req: Request) {
+  const token = await getToken({
+    req,
+    secret: process.env.AUTH_SECRET || "",
+  });
+
+  const isLoggedIn = !!token; // Check if the token exists
+  const url = new URL(req.url);
   const isProtectedRoute = protectedRoutes.some((route) =>
-    req.nextUrl.pathname.startsWith(route)
-  )
+    url.pathname.startsWith(route)
+  );
 
   if (isProtectedRoute && !isLoggedIn) {
-    return NextResponse.redirect(new URL("/", req.url))
+    return NextResponse.redirect(new URL("/", req.url)); // Redirect to homepage if not authenticated
   }
 
-  return NextResponse.next()
-})
+  return NextResponse.next(); // Proceed if authenticated or route is not protected
+}
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-}
+};

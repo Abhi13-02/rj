c5348c8e376a7a39fd50329908d2b567d71b9896
Pritam Roy <ratsdust4226@gmail.com>
2024-12-29 17:15:18 +0530

    cart api order updated

--- src/app/api/(orders)/orders/cart/route.ts
+++ src/app/api/(orders)/orders/cart/route.ts
@@ -12,32 +12,16 @@ export async function POST(req: NextRequest) {
     const body = await req.json();
     const { userId, shippingAddress, paymentMethod, shiprocketOrderId, status, items, totalAmount } = body;
 
-    // Find the user's cart
-    const userCart = await Cart.findOne({ userId }).populate('items.productId');
-
+    if(!userId || !shippingAddress || !paymentMethod || !items || !totalAmount || !shiprocketOrderId || !status) {
+      return NextResponse.json({ error: 'All fields are required' }, { status: 400 });
+    }
+    
     if(!paymentMethod) {
       return NextResponse.json({ error: ' payment method are required' }, { status: 400 });
     }
 
-   // Validate stock for each item
-   let messages = [];
-    for (const item of userCart.items) {
-    const productSizes = item.productId.sizes as { size: string; stock: number }[];
-    if (item.quantity > productSizes.filter((size ) => size.size === item.size)[0].stock) {
-      messages.push(
-        `Insufficient stock for ${item.productId.title}. Available: ${productSizes.filter((size ) => size.size === item.size)[0].stock}, Requested: ${item.quantity}`
-      );
-     }
-    }
-
-  // If there are any stock issues, return the messages
-  if (messages.length > 0) {
-    return NextResponse.json(
-      { error: 'Insufficient stock for some items', details: messages },
-      { status: 202 }
-    );
-  }
-
+    // Find the user's cart
+    // const userCart = await Cart.findOne({ userId });
     // Create the order
     const order = await Order.create({
       userId,
@@ -50,18 +34,19 @@ export async function POST(req: NextRequest) {
     });
 
     // Deduct stock for each product
-      for (const item of userCart.items) {
+      for (const item of items) {
         await Products.updateOne(
-          { _id: item.productId._id, "sizes.size": item.size }, // Match the product and size
+          { _id: item.productId, "sizes.size": item.size }, // Match the product and size
           { $inc: { "sizes.$.stock": -item.quantity } } // Decrement the stock for the matched size
         );
       }
 
-    // Clear the cart
-    userCart.items = [];
-    userCart.totalAmount = 0;
-    await userCart.save();
-
+    // // Clear the cart
+    // if(userCart){
+    //   userCart.items = [];
+    //   userCart.totalAmount = 0;
+    //   await userCart.save();
+    // }
     // Add order reference to the user
     await User.findByIdAndUpdate(userId, { $push: { yourOrders: order._id } });
 
